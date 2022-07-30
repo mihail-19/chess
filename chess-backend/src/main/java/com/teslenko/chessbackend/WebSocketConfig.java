191d0f23f354aecc8af170a9614440161834a7e8
mihail-19 <mihail.teslenko19@gmail.com>
2022-07-30 22:52:35 +0300

    cors url stomp

--- chess-backend/src/main/java/com/teslenko/chessbackend/WebSocketConfig.java
+++ chess-backend/src/main/java/com/teslenko/chessbackend/WebSocketConfig.java
@@ -1,5 +1,6 @@
 package com.teslenko.chessbackend;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
@@ -9,6 +10,8 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer{
+	@Value("${chess.cors.url}")
+	private String corsUrl;
 	
 	@Override
 	public void configureMessageBroker(MessageBrokerRegistry registry) {
@@ -20,7 +23,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer{
 	public void registerStompEndpoints(StompEndpointRegistry registry) {
 		registry
 			.addEndpoint("/stomp-endpoint")
-			.setAllowedOrigins("http://:3000")
+			.setAllowedOrigins(corsUrl)
 			.withSockJS();
 	}
 	
